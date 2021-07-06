da2078f527cec3087ee366a928df5fb271d602ce
chii <chishio.tachibana7300@gmail.com>
2021-07-06 18:28:02 +0400

    modulisation

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # epq-CellImageRecognition
- This is the programming hub for my EPQ artefact on using Machine Learning to Idenitfy Blood Cells utilising ResNet technology.
+ This is the programming hub for my EPQ artefact on using Machine Learning to Identify Blood Cells utilising ResNet-50 technology.
+
+ Each section is self explanatory (hopefully).
+
+ Within the model folder, the files for my model as well as the different versions that I have trained and saved.
+
+ Within the images folder, the dataset I use is stored - separated into 3 different sections: Training, Testing and Test-simple (which I will be using for validation).
--- model/model.py
+++ model/model.py
@@ -1,4 +1,6 @@
+'''
 <<<<<<< HEAD
+'''
 #imports for tf and resnet
 import tensorflow as tf
 from tensorflow import keras
@@ -15,8 +17,7 @@ import seaborn as sn
 from pathlib import Path
 from datetime import datetime
 from fsplit.filesplit import Filesplit
-from mlxtend.plotting import confusion_matrix as cm
-
+from mlxtend.evaluate import confusion_matrix as cm
 
 #img handling
 import PIL
@@ -35,119 +36,146 @@ classes = ['EOSINOPHIL', 'LYMPHOCYTE', 'MONOCYTE', 'NEUTROPHIL']
 #this is reversed order for the heatmap
 rclasses = ['NEUTROPHIL', 'MONOCYTE', 'LYMPHOCYTE', 'EOSINOPHIL']
 
-#first step for preprocessing - this uses the preprocessing input imported from the ResNet library.
-trainDGen = ImageDataGenerator(
-            preprocessing_function = preprocess_input,
-            )
-
-#fitting images to size as well as specifing the batch size and class mode.
-trainGen = trainDGen.flow_from_directory(
-            trainDir,
-            target_size = maxsize,
-            batch_size = 32,
-            class_mode = 'sparse',
-            )
-
-#fitting testing images in a similar format.
-testGen = trainDGen.flow_from_directory(
-            testDir,
-            target_size = maxsize,
-            batch_size = 1,
-            class_mode = 'sparse',
-            )
-
-#model - including the top dense layers for classification, as we are not using ImageNet weights.
-model = ResNet50(
-                include_top = True,
-                weights = None,
-                classes = 4,
+def loadImages(trainDir, testDir):
+    #first step for preprocessing - this uses the preprocessing input imported from the ResNet library.
+    trainDGen = ImageDataGenerator(
+                preprocessing_function = preprocess_input,
                 )
 
-#compiling the model together. In future there is the possibility of removing the top layer and adding our own dense layers.
-model.compile(
-            optimizer = AdaBound(lr=1e-3, final_lr=0.1),
-            loss = ['sparse_categorical_crossentropy'],
-            metrics = ['accuracy']
+    #fitting images to size as well as specifing the batch size and class mode.
+    trainGen = trainDGen.flow_from_directory(
+                trainDir,
+                target_size = maxsize,
+                batch_size = 32,
+                class_mode = 'sparse',
+                )
+
+    #fitting testing images in a similar format.
+    testGen = trainDGen.flow_from_directory(
+                testDir,
+                target_size = maxsize,
+                batch_size = 1,
+                class_mode = 'sparse',
+                )
+
+    #returns the generated dictionaries
+    return trainGen, testGen
+
+def firstModelSetup():
+    #model - including the top dense layers for classification, as we are not using ImageNet weights.
+    model = ResNet50(
+                    include_top = True,
+                    weights = None,
+                    classes = 4,
+                    )
+
+    #compiling the model together. In future there is the possibility of removing the top layer and adding our own dense layers.
+    model.compile(
+                optimizer = AdaBound(lr=1e-3, final_lr=0.1),
+                loss = ['sparse_categorical_crossentropy'],
+                metrics = ['accuracy']
+                )
+    #returns the freshly baked model
+    return model
+
+def trainModel(model, trainGen, epochs):
+    model.fit(
+            trainGen,
+            epochs = epochs
             )
 
-model.fit(
-        trainGen,
-        epochs = 10
-        )
+    #returns the trained model
+    return model
 
-#creating a path with the exact datetime
-now = datetime.now()
-savepath = now.strftime("model_%d%m%Y-%H%M")
-os.mkdir("model\saves\{}".format(savepath))
+def saveModel(model):
+    #if loading model in the same instance as saving
+    global savepath
+    #creating a path with the exact datetime.
+    now = datetime.now()
+    savepath = now.strftime("model_%d%m%Y-%H%M")
+    os.mkdir("model\saves\{}".format(savepath))
 
-#THIS IS TOO LARGE TO PUSH TO GITHUB. on its own
-#saving the model
-model.save(
-        ("model\saves\{}\model".format(savepath)),
-        include_optimizer = True,
-        save_format='h5'
-        )
+    #THIS IS TOO LARGE TO PUSH TO GITHUB. on its own
+    #saving the model in a h5 format
+    model.save(
+            ("model\saves\{}\model".format(savepath)),
+            include_optimizer = True,
+            save_format='h5'
+            )
 
-#splitting the file so that it can be pushed
-fs = Filesplit()
-fs.split(
-        ("model\saves\{}\model".format(savepath)),
-        10000000,
-        ("model\saves\{}".format(savepath))
-        )
+    #splitting the file so that it can be pushed
+    reSplit(savepath)
 
-#removing the big file
-os.remove(("model\saves\{}\model".format(savepath)))
+def loadModel(savepath):
+    #trying if model is in parts
+    try:
+        #merging the file back
+        fs = Filesplit()
+        fs.merge(
+                "model\saves\{}".format(savepath),
+                ("model\saves\{}\model".format(savepath)),
+                cleanup = True
+                )
+    except:
+        #if the model is already one file
+        pass
+    #loading the model at the previous datetime. When Loading, set compile to false due to using a custom optimizer that is not natively recognised.
+    model = keras.models.load_model("model\saves\{}\model".format(savepath), compile=False)
 
-#merging the file back
-fs.merge(
-        "model\saves\{}".format(savepath),
-        ("model\saves\{}\model".format(savepath)),
-        cleanup = True
-        )
+    #compiling the model together. In future there is the possibility of removing the top layer and adding our own dense layers.
+    model.compile(
+                optimizer = AdaBound(lr=1e-3, final_lr=0.1),
+                loss = ['sparse_categorical_crossentropy'],
+                metrics = ['accuracy']
+                )
+
+    #returning the model so it can be used
+    return model
+
+def evalModel(model, testGen):
+    #evaluating accuracy. verbose set to 2 for silence and information.
+    test_loss, test_acc = model.evaluate(testGen, verbose=2)
+    print('\nTest accuracy:', test_acc)
+
+    #returns the accuracy value to be compared
+    return test_acc
+
+
+def reSplit(savepath):
+    #splitting the file so that it can be pushed
+    fs = Filesplit()
+    fs.split(
+            ("model\saves\{}\model".format(savepath)),
+            10000000,
+            ("model\saves\{}".format(savepath))
+            )
+
+    #removing the big file
+    os.remove(("model\saves\{}\model".format(savepath)))
+
+def confMatrix(model, testGen, classes):
+    #reversing the classes
+    rclasses = classes[::-1]
+    #predicting the test set
+    predicts = model.predict(testGen)
+    yPredicted = np.argmax(predicts, axis=1)
+    #looping the testGen to obtain the true labels, then setting it as an array
+    test_labels = []
+
+    for i in range(0, testGen.samples):
+        test_labels.extend(np.array(testGen[i][1]))
+
+    yActual = np.asarray(test_labels, dtype=int)
+
+    #setting up a matrix using the mlxtend library
+    matrix = cm(yActual, yPredicted)
+
+    #plotting the heatmap using seaborn
+    #red colour, annotating the cells with their values and annotating the x and y classes with their variables.
+    sn.heatmap(matrix, cmap='Reds', annot=True, fmt='d', xticklabels = classes, yticklabels = rclasses)
 
 '''
-#save model as json
-modJson = model.to_json()
-
-#creating a path with the exact datetime
-now = datetime.now()
-savepath = now.strftime("model_%d%m%Y-%H%M")
-os.mkdir("model\saves\{}".format(savepath))
-
-with open(("model\saves\{}\config.json".format(savepath)), "wt") as f:
-    f.write(modJson)
-    model.save_weights("model\saves\{}\weights.hd5".format(savepath))
-f.close()
-'''
-
-
-
-#evaluating accuracy. verbose set to 2 for silence and information.
-test_loss, test_acc = model.evaluate(testGen, verbose=2)
-print('\nTest accuracy:', test_acc)
-
-#loading the model at the previous datetime. When Loading, set compile to false due to using a custom optimizer that is not natively recognised.
-model2 = keras.models.load_model("model\saves\{}\model".format(savepath), compile=False)
-
-#predicting the test set
-predicts = model.predict(testGen)
-yActual = np.asarray(test_labels, dtype=int)
-
-#looping the testGen to obtain labels, then setting it as an array
-test_labels = []
-
-for i in range(0, 71):
-    test_labels.extend(np.array(testGen[i][1]))
-
-np.asarray(test_labels, dtype=int64)
-
-#setting up a matrix
-matrix = cm(yActual, yPredicted)
-
-#plotting the heatmap
-sn.heatmap(matrix, cmap='Reds', annot=True, fmt='d', xticklabels = classes, yticklabels = rclasses)
-
 =======
 hello
 >>>>>>> parent of 3e79063 (first training + model complete)
+'''
